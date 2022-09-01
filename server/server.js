1d01fa947d7361e5760308af4bf513a56e808d3e
Golam Mostafa <mostafarmstu@gmail.com>
2022-09-02 03:10:12 +0600

    updated

--- server/server.js
+++ server/server.js
@@ -1,18 +1,12 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db_videos.json');
+const data = require("./db_videos.json");
 
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
-const PORT = process.env.PORT || 8000;
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-);
 server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running on', PORT);
-});
+
+server.listen(port);
